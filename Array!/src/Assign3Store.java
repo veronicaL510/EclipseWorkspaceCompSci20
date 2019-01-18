6d0ad5815de5051e0b2188307e8a24e75c9e3121
veronicaL5 <veronicaL5@091DS-1182-24.student.cssd.ab.ca>
2019-01-18 12:00:22 -0700

    gfgf

--- Array!/src/Assign3Store.java
+++ Array!/src/Assign3Store.java
@@ -5,58 +5,123 @@ public class Assign3Store {
 	Scanner scanNums = new Scanner(System.in); // scanner object to take in numbers
 	
 	// global variables 
-	public static String [] product = {"dog", "cat", "bird", "fish", "snake", "teacup pig", "baby elephant", "guinea pig", "rabbit", "chicken"};
-	static int [] prices = {1000, 500, 50, 30, 50, 150, 20000, 30, 40, 30};
-	static int [] itemNum = {};
-
-	{
-		
-	for (int i = 0; i < product.length; i++) {
+	public static String product [] = {"Dog", "Cat", "Bird", "Fish", "Snake", "Teacup pig", "Baby elephant", "Guinea pig", "Rabbit", "Chicken"};
+	public static double prices [] = {1000.00, 500.00, 50.00, 30.00, 50.00, 150.00, 20000.00, 30.00, 40.00, 30.00};
+	public static int itemNum []  = new int [10];
+	public static double discount = 1.00;
+	public static double totalPrice = 0;
+	
+	public static void listOfProducts() {
+		for (int i = 0; i < product.length; i++) {
 		System.out.println(i + ". " + product[i] + " and it costs " + prices[i] + " CAD.                                                                    ");
 	}
 	
-	int itemNumb;
-	int totalQty;
-	
-	}
+	}	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner scanNums = new Scanner(System.in); // scanner object to take in numbers
+		int y = 0;
 		
-		
-		
-		System.out.print("Please enter the number of the product you would like to purchase: "); // prompts user to type name
-		itemNumb = scanNums.nextInt(); // declares itemNumb variable and stores input
-		
-		if(itemNumb != 0) // not equal to 0
-			System.out.println("How many would you like? "); // prompts the user to enter how many items they would like
-		  int qty = scanNums.nextInt(); // stores what the next input will be
-			totalQty += qty; 
+		do {
+			System.out.println("Hi there! What would you like to buy?");
+			System.out.println(" ");
+			listOfProducts();
+			System.out.println("Type the number of the product you would like to get.");
+			Scanner choice = new Scanner(System.in);
+			int wantedProduct = choice.nextInt();
 			
-			if (itemNumb == 1) {
-				itemNum = prices[0] * qty; 
+			if (10 > wantedProduct && wantedProduct > 0) {
+				System.out.println("How many are you going to get?");
+				Scanner choice2 = new Scanner (System.in);
+				int numberOfProduct = choice2.nextInt();
+				itemNum [wantedProduct] = numberOfProduct;
+				double finalAmount = itemNum [wantedProduct] * prices [wantedProduct] * discount;
+				System.out.println(itemNum [wantedProduct] + " " + product[wantedProduct] + " would be $" + finalAmount + ".");
 				
+			}
+				
+			else if (wantedProduct == -1) {
+				System.out.println("CONGRATS YOU GOT A 10% DISCOUNT!");
+				discount = 0.90;
 				
 			}
-			else if (itemNumb == 2) {
-				price += calcCost(catPrice, qty); // computes how much the price will be if user chooses cat
-				catQty += qty;
-			}
-			else if (itemNumb == 3) {
-				price += calcCost(birdPrice, qty); // computes how much the price will be if user chooses bird
-				birdQty += qty;
-			}
+			
 			else {
-				System.out.println("We're sorry.  We do not provide that."); // what will be printed out into the console if the user enters a digit that is not 1 or 2 or 3
+				System.out.println("Sorry about that but I don't think we have that.");
+				
 			}
-		} while(itemNumb != 0); 
-}
-		public static double calcCost(double itemPrice, int qty) {
-			return itemPrice * qty;
-		} // end calcCost
-	
-	}
+			
+			System.out.println("Are you looking for anything else? Type the corresponding number.");
+			System.out.println("1. Yes");
+			System.out.println("2. No");
+			
+			Scanner choice3 = new Scanner (System.in);
+			int question = choice3.nextInt();
+			
+			if (question == 1) {
+				
+			}
+			
+			else if (question == 2) {
+				y = 1;
+			}
+			
+			else {
+				System.out.println("Let's go anyway :) ");
+			}
+			
+		} while (y == 0);
+		
+		System.out.println(" ");
+		
+		for (int i = 0; i < product.length; i++) {
+			if (itemNum[i] > 0) {
+				double eachPriceTotal = prices[i] * itemNum[i] * discount;
+				System.out.println("Product Price Num Bought Total");
+				System.out.printf(product[i] + "     " + prices[i] + "     " + itemNum[i] + "     " + eachPriceTotal);
+				System.out.println("\n_________________________________________");
+				System.out.println("Your total is $" + eachPriceTotal + ".  Come back again!");
+			}
+			
+		}
 
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- tutorial/src/C_C/Caesar_Cipher.java
+++ tutorial/src/C_C/Caesar_Cipher.java
@@ -3,27 +3,57 @@ import java.util.Scanner;
 
 public class Caesar_Cipher {
 
-	public static String encrypt (String plainText, int shiftValue) {
+	// Scanners for statements and numbers
+	static Scanner scanstatements = new Scanner(System.in);
+	static Scanner scannums = new Scanner(System.in);
+	
+	public static String encrypt(String encryptedText, int shiftValue)
+	{
+	
+		String output = "";
 		
-	}
+		// changes letter to a char value and shifts values of char to encrypt and returns it to string
+		for(int i = 0; i < encryptedText.length(); i ++) {
+			char encrypted = (char) (encryptedText.charAt(i) + shiftValue); 
+			output += String.valueOf(encrypted);
+		}
+		
+		// return the output
+		return output;
+		
+	} 
 
-	public static String decrypt (String encryptedText, int shiftValue) {
+	public static String decrypt(String encryptedText, int shiftValue)	{
 		
-	}
+		String output = "";
+		
+		// changes letter to a char value and shifts values of char to decrypt and returns it to string
+		for(int i = 0 ; i < encryptedText.length() ; i ++) {
+			char dencrypted = (char) (encryptedText.charAt(i) - shiftValue); 
+			output += String.valueOf(dencrypted);
+		}
+		
+		// return output
+		return output;
+	} 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner scanWords = new Scanner(System.in); // scanner object to take in words
+		// asks the user for something to encrypt
+		System.out.println("Hey there player!  Type something!  No punctuation please and thanks!  We're going to encrypt it.");
+		// store statement in a variable and encrypt it 
+		String statement = encrypt(scanstatements.nextLine(),5);
+		// print the encrypted statement
+		System.out.println("That statement encrypted is '" + statement + "'.");
 		
-		System.out.print("Hey there player!  Type something!  No punctuation please and thanks!");
-		String text = scanWords.nextLine();
-		
-		System.out.println(encrypt(text, 5));
-		System.out.println(decrypt());
-		
+		// decrypt the statement again and store in a variable
+		System.out.println("\nLet's decrypt it meaning put it in it's original statement.");
+		statement = decrypt(statement,5);
+		// print the decrypted statement
+		System.out.println("The statement decrypted is '" + statement + "'.");
+			
 	}
 
-}
+} 
+
 
 	
